5c13de57e5f3558d35be92147bcb0fcfa99fb415
tpnbolwerk <twanbolwerk@gmail.com>
2021-09-06 19:54:53 +0200

    client, server and simple gui

---
+++ src/main/java/spl/Server.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package spl;
+
+/**
+ * @author twanbolwerk
+ *
+ */
+public class Server {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
