39567bbf0b04cf0872849bb31da2364584840e29
Stef Thijssen <stef.thijssen@proforto.nl>
2021-09-16 11:40:59 +0200

    Add feature selection

--- RuntimeParameterChatApp/src/main/java/client/FeatureConfiguration.java
+++ RuntimeParameterChatApp/src/main/java/client/FeatureConfiguration.java
@@ -1,5 +1,7 @@
 package client;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 public class FeatureConfiguration {
 	public boolean color;
 	public boolean encryption;
@@ -11,10 +13,18 @@ public class FeatureConfiguration {
 	}
 
 	private void getConfiguration() {
-		// TODO: Get from config file
-		color = false;
-		encryption = false;
-		authentication = false;
-		logging = false;
+		try {
+			Dotenv dotenv = Dotenv.load();
+			color = Boolean.parseBoolean(dotenv.get("COLOR"));
+			encryption = Boolean.parseBoolean(dotenv.get("ENCRYPTION"));
+			authentication = Boolean.parseBoolean(dotenv.get("AUTHENTICATION"));
+			logging = Boolean.parseBoolean(dotenv.get("LOGGING"));
+		} catch (Exception e) {
+			System.out.println("Error: Something went wrong with the .env file. All features are turned of by default");
+			color = false;
+			encryption = false;
+			authentication = false;
+			logging = false;
+		}
 	}
 }
