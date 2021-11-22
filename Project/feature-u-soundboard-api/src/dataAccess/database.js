0a581c25849cf6f12d616d3aa1b6bd834f10ef6c
SERDANS <serdans@protonmail.com>
2021-11-22 11:15:38 +0100

    wip db

--- Project/feature-u-soundboard-api/src/dataAccess/database.js
+++ Project/feature-u-soundboard-api/src/dataAccess/database.js
@@ -2,7 +2,7 @@
 exports.__esModule = true;
 exports.DB_FILE = void 0;
 var sqlite3_1 = require("sqlite3");
-exports.DB_FILE = "soundboard-db";
+exports.DB_FILE = "soundboard";
 var useDb = function (fn) {
     var db = new sqlite3_1["default"].Database(exports.DB_FILE);
     var result = fn(db);
