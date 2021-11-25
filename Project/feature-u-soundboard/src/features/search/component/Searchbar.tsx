8db85a4f21af7db150e07fba35b289093c02fc27
tpnbolwerk <twanbolwerk@gmail.com>
2021-11-25 16:27:15 +0100

    search implemented

--- Project/feature-u-soundboard/src/features/search/component/Searchbar.tsx
+++ Project/feature-u-soundboard/src/features/search/component/Searchbar.tsx
@@ -1,12 +1,84 @@
+/* eslint-disable @typescript-eslint/no-unsafe-argument */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
 /* eslint-disable react/jsx-no-undef */
-import React from 'react';
-import IconButton from '@mui/material/IconButton';
+import * as React from 'react';
+import { styled, alpha } from '@mui/material/styles';
+import InputBase from '@mui/material/InputBase';
 import SearchIcon from '@mui/icons-material/Search';
+/*
+Search is implemented in such a way every component can just parse the url to get the search parameters and do whatever needs to be done for that component
 
+using the following code for parsing the url:
+
+const urlSearchParams = new URLSearchParams(window.location.search);
+const params = Object.fromEntries(urlSearchParams.entries());
+console.log(params) // {search: ''}
+*/
 export default function Searchbar() {
+  const Search = styled('div')(({ theme }) => ({
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: alpha(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: alpha(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(1),
+      width: 'auto',
+    },
+  }));
+
+  const SearchIconWrapper = styled('div')(({ theme }) => ({
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  }));
+
+  const StyledInputBase = styled(InputBase)(({ theme }) => ({
+    color: 'inherit',
+    '& .MuiInputBase-input': {
+      padding: theme.spacing(1, 1, 1, 0),
+      // vertical padding + font size from searchIcon
+      paddingLeft: `calc(1em + ${theme.spacing(4)})`,
+      transition: theme.transitions.create('width'),
+      width: '100%',
+    },
+  }));
+  const urlSearchParams = new URLSearchParams(window.location.search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  const [search, setSearch] = React.useState(params.search ?? '');
+
+  const onChange = (e) => {
+    e.preventDefault();
+    const value: string = e.target.value;
+    setSearch(value);
+  };
+
+  const onSubmit = (e) => {
+    if (e.key === 'Enter') {
+      window.location.assign(`?search=${search}`);
+    }
+  };
+
   return (
-    <IconButton>
-      <SearchIcon />
-    </IconButton>
+    <Search>
+      <SearchIconWrapper>
+        <SearchIcon />
+      </SearchIconWrapper>
+      <StyledInputBase
+        placeholder="Search…"
+        inputProps={{ 'aria-label': 'search' }}
+        value={search}
+        onChange={(e) => onChange(e)}
+        autoFocus={true}
+        onKeyDown={(e) => onSubmit(e)}
+      />
+    </Search>
   );
 }
