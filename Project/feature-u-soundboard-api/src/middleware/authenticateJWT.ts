384f83cddfdb50ce301dc9b6a1d59361a5eb045d
tpnbolwerk <twanbolwerk@gmail.com>
2021-11-26 13:07:30 +0100

    make env use the same issues in both api and frontend

--- Project/feature-u-soundboard-api/src/middleware/authenticateJWT.ts
+++ Project/feature-u-soundboard-api/src/middleware/authenticateJWT.ts
@@ -6,10 +6,10 @@ export const authenticateJWT = jwt({
         cache: true,
         rateLimit: true,
         jwksRequestsPerMinute: 5,
-        jwksUri: `${process.env.AUTH0_ISSUER}/.well-known/jwks.json`
+        jwksUri: `https://${process.env.AUTH0_ISSUER}/.well-known/jwks.json`
     }),
     audience: process.env.AUTH0_CLIENT_ID,
-    issuer: [`${process.env.AUTH0_ISSUER}/`],
+    issuer: [`https://${process.env.AUTH0_ISSUER}/`],
     algorithms: ['RS256']
 });
 
