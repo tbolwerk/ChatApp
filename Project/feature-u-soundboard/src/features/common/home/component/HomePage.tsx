cff122c56319102000736a7790df5007651b53ec
SERDANS <serdans@protonmail.com>
2021-11-28 10:34:06 +0100

    add upload and container to homepage

--- Project/feature-u-soundboard/src/features/common/home/component/HomePage.tsx
+++ Project/feature-u-soundboard/src/features/common/home/component/HomePage.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import { IRoute } from '../../../../interfaces/IRoute';
+import { useFassets } from 'feature-u';
 
-const HomePage = () => <h1>Home</h1>;
+const HomePage = () => {
+  const SoundContainer = useFassets('play.soundContainer');
+  const SoundForm = useFassets('upload.form');
+
+  return (
+    <>
+      <h1>Home</h1>
+      <SoundForm />
+      <SoundContainer />
+    </>
+  );
+};
 
 export default HomePage;
