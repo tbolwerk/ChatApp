f2c213d87f95b3a55cc6124ae6a8c6bc19352885
SERDANS <serdans@protonmail.com>
2021-12-02 17:32:40 +0100

    remove homepgae voice

--- Project/feature-u-soundboard/src/features/common/home/component/HomePage.tsx
+++ Project/feature-u-soundboard/src/features/common/home/component/HomePage.tsx
@@ -3,9 +3,6 @@ import { useFassets } from 'feature-u';
 
 const HomePage = () => {
   const Sounds = useFassets('sounds.SoundOverview');
-  const Voice = useFassets('voice.form');
-
-  return <Voice />;
 
   return Sounds ? <Sounds /> : <h1>Home</h1>;
 };
