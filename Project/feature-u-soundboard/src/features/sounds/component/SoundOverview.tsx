daa0e1416f667d8e36230429e6d57db5f2cd9c9c
tpnbolwerk <twanbolwerk@gmail.com>
2021-12-04 17:52:43 +0100

    pagination added to categories and sound

--- Project/feature-u-soundboard/src/features/sounds/component/SoundOverview.tsx
+++ Project/feature-u-soundboard/src/features/sounds/component/SoundOverview.tsx
@@ -13,17 +13,22 @@ export default function SoundOverview({ category }) {
     { title: 'sirene' },
   ];
 
+  const entriesPerPage = 3;
+
   const urlSearchParams = new URLSearchParams(window.location.search);
   const params = Object.fromEntries(urlSearchParams.entries());
   const filteredSounds = sounds.filter(
     (x) =>
       params.search === undefined || x.title.toUpperCase().includes(params.search.toUpperCase()),
   );
+  const start: number = parseInt(params.page, 10) ?? 1;
+  const end = start + entriesPerPage;
+  const pagedSounds = filteredSounds?.slice(start - 1, end - 1) ?? [];
 
   const PaginationFeature = useFassets('pagination.PaginationFeature');
   return (
     <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
-      {filteredSounds.map((sound, index) => (
+      {pagedSounds.map((sound, index) => (
         <Grid item xs={2} sm={4} md={4} key={index}>
           <MediaControlCard
             title={sound.title}
