b63071ee59e2623490eba8ccfc9803a446dde617
SERDANS <serdans@protonmail.com>
2021-12-06 12:00:28 +0100

    fix tts api

--- Project/feature-u-soundboard-api/src/controllers/soundController.ts
+++ Project/feature-u-soundboard-api/src/controllers/soundController.ts
@@ -1,4 +1,9 @@
 import { getSounds, insertSound, updateFavorite, getAllSounds } from "../dataAccess/soundDAO";
+import {getAudioUrl} from "google-tts-api";
+import Axios from "axios";
+import {type} from "os";
+import { Blob } from 'buffer';
+import fs from 'fs';
 
 class SoundController {
     get(username:string) {
@@ -16,6 +21,29 @@ class SoundController {
     getAll(){
         return getAllSounds();
     }
+
+    async getTTS(text: string) {
+        try {
+            const url = getAudioUrl(text);
+            const res = await Axios.get(url, {
+                responseType: "arraybuffer"
+            });
+            console.log(url)
+            console.log(typeof res.data)
+            // const stream = fs.createWriteStream("test.mp3");
+            // const blob = new Blob([res.data], {
+            //     type: 'audio/mpeg'
+            // });
+            // stream.write(res.data);
+            // stream.end();
+            const buf = Buffer.from(res.data, 'binary');
+            fs.writeFileSync(`${encodeURIComponent(text)}.mp3`, buf);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+
 }
 
 const controller = new SoundController();
