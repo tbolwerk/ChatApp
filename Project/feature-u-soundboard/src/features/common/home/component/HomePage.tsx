06c52d2293979fefdbc2df7fd20b74442c4b9a80
SERDANS <serdans@protonmail.com>
2021-12-06 14:13:00 +0100

    remove tts from homepage

--- Project/feature-u-soundboard/src/features/common/home/component/HomePage.tsx
+++ Project/feature-u-soundboard/src/features/common/home/component/HomePage.tsx
@@ -3,9 +3,6 @@ import { useFassets } from 'feature-u';
 
 const HomePage = () => {
   const Sounds = useFassets('sounds.SoundOverview');
-  const TTS = useFassets('tts.form');
-
-  return <TTS />;
 
   return Sounds ? <Sounds /> : <h1>Home</h1>;
 };
